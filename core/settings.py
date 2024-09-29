756ea21eb0224838989c994689676cec54b9e12e
Mohammadreza-Alizadeh <alizadehmohamadreza641@gmail.com>
2024-09-29 05:04:35 +0330

    added api app

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+LOCAL_APPS = [
+    'applications.api.apps.ApiConfig',
+]
+
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -41,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    *LOCAL_APPS,
 ]
 
 MIDDLEWARE = [
