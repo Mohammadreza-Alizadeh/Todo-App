9a68cff2118e0d18f65cec65fd3239f6ed6d322d
Mohammadreza-Alizadeh <alizadehmohamadreza641@gmail.com>
2024-10-06 05:28:51 +0330

    Created Task model

--- applications/tasks/models.py
+++ applications/tasks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from applications.common.models import BaseMdoel
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class Task(BaseMdoel):
+
+    priority_choices = [
+        (1, 'Low'),
+        (2, 'Important'),
+        (3, 'Critical'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
+    title = models.CharField(verbose_name='Task Title', max_length=128)    
+    description = models.CharField(verbose_name='Task Description', max_length=128, blank=True, null=True)
+    priority = models.IntegerField(verbose_name='Task Priority' ,choices=priority_choices, default=1) 
+    is_done = models.BooleanField(verbose_name='Task Status ( is done )',  default=False)
+
+    def __str__(self) -> str:
+        return f'{self.title}' 
+    
